1b4744f7efb25552d9f3ecc597634babc43ca976
Junhyeong <wnsgudqkr718@gmail.com>
2022-11-05 15:09:29 +0900

    코드 구조 개선

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
 # Program make a simple calculator
 
-# This function adds two numbers
-def add(x, y):
-    return x + y
-
-# This function subtracts two numbers
-def subtract(x, y):
-    return x - y
-
-
-# This function multiplies two numbers
-def multiply(x, y):
-    return x * y
-
-#Need to define divide function.
-def divide (x,y):
-    return x/y
+from calc_lib import *
 
 print("Calculator started.")
 
