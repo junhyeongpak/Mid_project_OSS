aaeb2d47bbeb13c54258d2a6e026fe0c0b75d0c1
Junhyeong <wnsgudqkr718@gmail.com>
2022-11-05 15:22:02 +0900

    정상적인 계산, 비정상 루틴에 대해서 로그파일 기록 기능 추가

--- main.py
+++ main.py
@@ -19,25 +19,43 @@ while True:
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
+        
+        # 로그 파일 기록 시작
+        log_f = open("log.txt", "a")
+        
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            result = add(num1, num2) #계산 결과 기록
+            print(num1, "+", num2, "=", result)
+            msg = "The result is " + str(result)+ '\n'
+            log_f.write(msg) # 계산 결과를 로그파일에 기록
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            result = subtract(num1, num2)
+            print(num1, "-", num2, "=", result)
+            msg = "The result is " + str(result)+ '\n'
+            log_f.write(msg) # 계산 결과를 로그파일에 기록
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            result = multiply(num1, num2)
+            print(num1, "*", num2, "=", result)
+            msg = "The result is " + str(result)+ '\n'
+            log_f.write(msg) # 계산 결과를 로그파일에 기록
+            
             
         elif choice =='4':
             # 분모에 0이 들어오는 경우 계산을 실행하이 않는다. 
             if num2 == 0:
                 print("Do not divide by zero!!!")
+                log_f.write("Do not divide by zero!!!\n") # 0으로 나눈 것을 로그파일에 기록
             # 0이 아닌 경우는 정상적으로 divide를 실행한다.
             else:
-                print(num1, "/", num2, "=", divide(num1,num2))
+                result = divide(num1,num2)
+                print(num1, "/", num2, "=", result)
+                msg = "The result is " + str(result)+ '\n'
+                log_f.write(msg) # 계산 결과를 로그파일에 기록
             
 
         # check if user wants another calculation
@@ -64,11 +82,20 @@ while True:
                 break
             # 그 외의 이상한 답변이 오는 경우 다시 Let's do next calc로 회귀
             else:
-                print("Please anwser...(yes/no)")
+                print("Please answer... (yes/no)")
+
+                # 잘못된 입력이 오는 경우 로그파일에 기록
+                log_f.write("Please answer... (yes/no)\n")
                 continue
         
         if finish_state == True:
+            log_f.write("Finish Calculation\n") # 계산이 끝난 것을 로그파일에 기록
+            log_f.write("---------------------------\n") # 다른 계산 루트와 구분
+            log_f.close() # 파일을 닫음
             break
 
     else:
         print("Invalid Input")
+        log_f = open("log.txt", "a") # 1/2/3/4 외의 입력이 오는 경우 파일을 열어줌
+        log_f.write("Invalid Input\n") # 잘못된 입력이 온 것을 로그파일에 기록
+        log_f.close() # 파일을 닫아줌
